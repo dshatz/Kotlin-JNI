89049fdea2a4a83e78b58a682beb7f926acfc40e
Daniels Šatcs <dev@dshatz.com>
2026-07-16 14:10:23 +0000

    Enable serialization for kotlin arrays

--- e2e/src/commonMain/kotlin/kni/test/serializable/Polymorphic.kt
+++ e2e/src/commonMain/kotlin/kni/test/serializable/Polymorphic.kt
@@ -7,5 +7,26 @@ import kni.test.Callback
 sealed interface PolymorphicFruit {
     data class Orange(val sweet: Boolean): PolymorphicFruit
     data class Apple(val color: Long): PolymorphicFruit
-    /*data class Alive(val callback: Callback): PolymorphicFruit*/
+    data class Watermelon(val contents: ByteArray, val longs: LongArray, val chars: CharArray): PolymorphicFruit {
+
+        override fun equals(other: Any?): Boolean {
+            if (this === other) return true
+            if (other == null || this::class != other::class) return false
+
+            other as Watermelon
+
+            if (!contents.contentEquals(other.contents)) return false
+            if (!longs.contentEquals(other.longs)) return false
+            if (!chars.contentEquals(other.chars)) return false
+
+            return true
+        }
+
+        override fun hashCode(): Int {
+            var result = contents.contentHashCode()
+            result = 31 * result + longs.contentHashCode()
+            result = 31 * result + chars.contentHashCode()
+            return result
+        }
+    }
 }
--- ksp/src/jvmMain/kotlin/com/dshatz/kni/serialization/SerializerProcessor.kt
+++ ksp/src/jvmMain/kotlin/com/dshatz/kni/serialization/SerializerProcessor.kt
@@ -5,16 +5,16 @@ import com.dshatz.kni.Types
 import com.dshatz.kni.annotations.JniSerializable
 import com.dshatz.kni.annotations.JniSerializerFor
 import com.dshatz.kni.kspfix.findAnnotation
-import com.dshatz.kni.kspfix.getArgumentValueByName
 import com.dshatz.kni.kspfix.getClassArgument
 import com.dshatz.kni.utils.dereferenceTypeAlias
+import com.google.devtools.ksp.getDeclaredProperties
 import com.google.devtools.ksp.processing.KSPLogger
 import com.google.devtools.ksp.processing.Resolver
 import com.google.devtools.ksp.processing.SymbolProcessorEnvironment
 import com.google.devtools.ksp.symbol.ClassKind
 import com.google.devtools.ksp.symbol.KSClassDeclaration
-import com.google.devtools.ksp.symbol.KSTypeReference
-import com.google.devtools.ksp.symbol.KSValueParameter
+import com.google.devtools.ksp.symbol.KSNode
+import com.google.devtools.ksp.symbol.KSPropertyDeclaration
 import com.google.devtools.ksp.symbol.Modifier
 import com.squareup.kotlinpoet.ClassName
 import com.squareup.kotlinpoet.CodeBlock
@@ -27,11 +27,8 @@ import com.squareup.kotlinpoet.ParameterizedTypeName.Companion.parameterizedBy
 import com.squareup.kotlinpoet.PropertySpec
 import com.squareup.kotlinpoet.TypeName
 import com.squareup.kotlinpoet.TypeSpec
-import com.squareup.kotlinpoet.ksp.originatingKSFiles
 import com.squareup.kotlinpoet.ksp.toClassName
 import com.squareup.kotlinpoet.ksp.toTypeName
-import java.io.Serial
-import kotlin.reflect.KClass
 
 class SerializerProcessor(
     private val registry: Registry,
@@ -46,13 +43,20 @@ class SerializerProcessor(
         resolver: Resolver
     ): Sequence<SerialClass> {
         fun collectSerialProps(decl: KSClassDeclaration): SerialClass.DataClass {
-            val props = decl.primaryConstructor!!.parameters.map {
+
+            val valParams = decl.primaryConstructor!!.parameters.map {
+                it.name!!.asString()
+            }.toSet()
+            val props = decl.getAllProperties()
+                .filter { it.simpleName.asString() in valParams }
+                .map {
                 SerialProp(
-                    it.name?.asString()!!,
+                    it.simpleName.asString(),
                     it.type.dereferenceTypeAlias().toTypeName(),
+                    declaration = it,
                     overrideSerializer = it.getOverrideSerializer()
                 )
-            }
+            }.toList()
             return SerialClass.DataClass(decl.toClassName(), props)
         }
 
@@ -76,8 +80,8 @@ class SerializerProcessor(
                 }
             }
             .filter { d ->
-                (Modifier.DATA in d.modifiers).also {
-                    if (!it) env.logger.warn("Ignoring @JniSerializable: not a data class", d)
+                (Modifier.DATA in d.modifiers || Modifier.VALUE in d.modifiers).also {
+                    if (!it) env.logger.warn("Ignoring @JniSerializable on a class: must have data or value modifiers.", d)
                 }
             }
             .map(::collectSerialProps)
@@ -85,11 +89,10 @@ class SerializerProcessor(
         return dataClasses + sealed
     }
 
-    private fun KSValueParameter.getOverrideSerializer(): ClassName? {
-        val paramValue = findAnnotation<JniSerializable>()?.getArgumentValueByName<KClass<*>>("with")
-        if (paramValue != null) {
-            return (paramValue as KSTypeReference).resolve().toClassName()
-        } else return null
+    private fun KSPropertyDeclaration.getOverrideSerializer(): ClassName? {
+        val annotation = findAnnotation<JniSerializable>()
+        val paramValue = annotation?.getClassArgument("with")
+        return paramValue
     }
 
     fun findSerializers(resolver: Resolver, env: SymbolProcessorEnvironment): Map<ClassName, ClassName> {
@@ -203,10 +206,15 @@ class SerializerProcessor(
     private fun packCode(props: List<SerialProp>): CodeBlock {
         val builder = CodeBlock.builder()
         props.map {
-            val serializer = included.serializer(it.type)
+            val serializer = try {
+                included.serializer(it.type, it.overrideSerializer)
+            } catch (e: NoSerializerException) {
+                logger.error(e.message!!, it.declaration)
+                throw e
+            }
             if (serializer is IncludedSerializers.Serializer.Generic) registry.genericSerializers.add(serializer)
             val statement = serializer.writeCode(CodeBlock.of("buffer"), CodeBlock.of("value.%N", it.name))
-            builder.add("\n%L", statement)
+            builder.add("\n%L\n", statement)
         }
         return builder.build()
     }
@@ -214,8 +222,13 @@ class SerializerProcessor(
     private fun unpackCode(info: SerialClass.DataClass): CodeBlock {
         val builder = CodeBlock.builder()
         info.properties.forEach {
-            val serializer = included.serializer(it.type)
-            builder.addStatement("val %N = %L", it.name, serializer.readCode(CodeBlock.of("buffer")))
+            val serializer = try {
+                included.serializer(it.type, it.overrideSerializer)
+            } catch (e: NoSerializerException) {
+                logger.error(e.message!!, it.declaration)
+                throw e
+            }
+            builder.add("\nval %N = %L\n", it.name, serializer.readCode(CodeBlock.of("buffer")))
         }
         return builder.addStatement("return %T(%L)", info.cls, info.properties.joinToString { it.name }).build()
     }
@@ -381,6 +394,7 @@ class SerializerProcessor(
     data class SerialProp(
         val name: String,
         val type: TypeName,
+        val declaration: KSNode,
         val overrideSerializer: ClassName? = null
     )
 
