9692f0d83df49483b8b3e30f6ab687d9dbe161d4
Daniels Šatcs <dev@dshatz.com>
2026-07-22 09:58:01 +0000

    Enable serialization for enums

---
+++ e2e/src/commonMain/kotlin/kni/test/serializable/ColorEnum.kt
@@ -0,0 +1,13 @@
+package kni.test.serializable
+
+import com.dshatz.kni.annotations.JniSerializable
+
+@JniSerializable
+enum class ColorEnum {
+    RED,
+    ORANGE,
+    YELLOW,
+    GREEN,
+    BLUE,
+    PURPLE
+}
